3f77212bd7464eb35246a74f76b0962e6063fcd5
Brayam Morando <bmorando@ant.com.mx>
2019-01-15 16:43:58 -0600

    admin 3

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -50,7 +50,7 @@
           <!-- Logo -->
           <a href="./" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>P</b>C</span>
+            <span class="logo-mini">HC</span>
             <!-- logo for regular state and mobile devices -->
             <span class="logo-lg">Helados Chap's</span>
           </a>
