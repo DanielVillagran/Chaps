73b4e590b0af9fba50ba7ea9808f78740ee2a7fd
Brayam Morando <bmorando@ant.com.mx>
2019-01-15 16:33:50 -0600

    admin color

--- core/app/layouts/layout.php
+++ core/app/layouts/layout.php
@@ -50,9 +50,9 @@
           <!-- Logo -->
           <a href="./" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
-            <span class="logo-mini"><b>C</b>M</span>
+            <span class="logo-mini"><b>P</b>C</span>
             <!-- logo for regular state and mobile devices -->
-            <span class="logo-lg"><b>Paleteria</b>Chap's</span>
+            <span class="logo-lg"><b>Paleteria</b> Chap's</span>
           </a>
 
           <!-- Header Navbar -->
