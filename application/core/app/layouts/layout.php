f6b0e43c89119346ecf6850e07e2520f2db1621f
DanielVillagran <odvillagrana@gmail.com>
2020-01-18 17:13:00 -0600

    Changes

--- application/core/app/layouts/layout.php
+++ application/core/app/layouts/layout.php
@@ -21,14 +21,13 @@
         <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
         <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
       <![endif]-->
-      <?php 
-      date_default_timezone_set('America/Mexico_City');
-      if( ! ini_get('date.timezone') )
-      {
-        date_default_timezone_set('GMT');
-      }
+      <?php
+date_default_timezone_set('America/Mexico_City');
+if (!ini_get('date.timezone')) {
+	date_default_timezone_set('GMT');
+}
 
-      ?>
+?>
       <script src="plugins/jquery/jquery-2.1.4.min.js"></script>
       <script src="plugins/morris/raphael-min.js"></script>
       <script src="plugins/morris/morris.js"></script>
@@ -36,17 +35,17 @@
       <link rel="stylesheet" href="plugins/morris/example.css">
       <script src="plugins/jspdf/jspdf.min.js"></script>
       <script src="plugins/jspdf/jspdf.plugin.autotable.js"></script>
-      <?php if(isset($_GET["view"]) && $_GET["view"]=="sell"):?>
+      <?php if (isset($_GET["view"]) && $_GET["view"] == "sell"): ?>
         <script type="text/javascript" src="plugins/jsqrcode/llqrcode.js"></script>
         <script type="text/javascript" src="plugins/jsqrcode/webqr.js"></script>
       <?php endif;?>
 
     </head>
 
-    <body class="<?php if(isset($_SESSION["user_id"]) || isset($_SESSION["client_id"])):?>  skin-green-light sidebar-mini <?php else:?>login-page<?php endif; ?>" >
+    <body class="<?php if (isset($_SESSION["user_id"]) || isset($_SESSION["client_id"])): ?>  skin-green-light sidebar-mini <?php else: ?>login-page<?php endif;?>" >
       <div class="wrapper">
         <!-- Main Header -->
-        <?php if(isset($_SESSION["user_id"]) || isset($_SESSION["client_id"])):?>
+        <?php if (isset($_SESSION["user_id"]) || isset($_SESSION["client_id"])): ?>
         <header class="main-header">
           <!-- Logo -->
           <a href="./" class="logo">
@@ -67,32 +66,34 @@
               <ul class="nav navbar-nav">
 
                 <?php
-                if(isset($_SESSION["user_id"])):
-                  $msgs = MessageData::getUnreadedByUserId($_SESSION["user_id"]);
-                  ?>
-                  <li class="dropdown messages-menu">
-                   
-                    <ul class="dropdown-menu">
-                     
-                      <li>
-                        <!-- inner menu: contains the actual data -->
-                        <ul class="menu">
-                          <?php foreach($msgs as $i):?>
-                            <li><!-- start message -->
-                              <a href="./?view=messages&opt=open&code=<?php echo $i->code;?>">
-                                <h4>
-                                  <?php if($i->user_from!=$_SESSION["user_id"]):?>
-                                    <?php $u = $i->getFrom(); echo $u->name." ".$u->lastname;?>
-                                    <?php elseif($i->user_to!=$_SESSION["user_id"]):?>
-                                      <?php $u = $i->getTo(); echo $u->name." ".$u->lastname;?>
-                                    <?php endif; ?>
+if (isset($_SESSION["user_id"])):
+	$msgs = MessageData::getUnreadedByUserId($_SESSION["user_id"]);
+	?>
+																								                  <li class="dropdown messages-menu">
+
+																								                    <ul class="dropdown-menu">
+
+																								                      <li>
+																								                        <!-- inner menu: contains the actual data -->
+																								                        <ul class="menu">
+																								                          <?php foreach ($msgs as $i): ?>
+																								                            <li><!-- start message -->
+																								                              <a href="./?view=messages&opt=open&code=<?php echo $i->code; ?>">
+																								                                <h4>
+																								                                  <?php if ($i->user_from != $_SESSION["user_id"]): ?>
+																								                                    <?php $u = $i->getFrom();
+	echo $u->name . " " . $u->lastname;?>
+																								                                    <?php elseif ($i->user_to != $_SESSION["user_id"]): ?>
+                                      <?php $u = $i->getTo();
+echo $u->name . " " . $u->lastname;?>
+                                    <?php endif;?>
                                     <small><i class="fa fa-clock-o"></i> 5 mins</small>
 
                                   </h4>
                                   <p><?php echo $i->message; ?></p>
                                 </a>
                               </li>
-                            <?php endforeach; ?>
+                            <?php endforeach;?>
 
                           </ul>
                         </li>
@@ -106,12 +107,11 @@
                     <a href="#" class="dropdown-toggle" data-toggle="dropdown">
                       <!-- The user image in the navbar-->
                       <!-- hidden-xs hides the username on small devices so only the image appears. -->
-                      <span class=""><?php if(isset($_SESSION["user_id"]) ){ echo UserData::getById($_SESSION["user_id"])->name; 
-                      if(Core::$user->kind==1){ echo " (Administrador)"; }
-                      else if(Core::$user->kind==2){ echo " (Almacenista)"; }
-                      else if(Core::$user->kind==3){ echo " (Vendedor)"; }
+                      <span class=""><?php if (isset($_SESSION["user_id"])) {
+	echo UserData::getById($_SESSION["user_id"])->name;
+	if (Core::$user->kind == 1) {echo " (Administrador)";} else if (Core::$user->kind == 2) {echo " (Coordinador)";} else if (Core::$user->kind == 3) {echo " (Vendedor)";}
 
-                    }else if (isset($_SESSION["client_id"])){ echo PersonData::getById($_SESSION["client_id"])->name." (Cliente)" ;}?> <b class="caret"></b> </span>
+} else if (isset($_SESSION["client_id"])) {echo PersonData::getById($_SESSION["client_id"])->name . " (Cliente)";}?> <b class="caret"></b> </span>
 
                   </a>
                   <ul class="dropdown-menu">
@@ -150,11 +150,13 @@
         <!-- Sidebar Menu -->
         <ul class="sidebar-menu">
           <li class="header">ADMINISTRACION</li>
-          <?php if(isset($_SESSION["user_id"])):?>
+          <?php if (isset($_SESSION["user_id"])): ?>
+             <?php if (Core::$user->kind == 1): ?>
             <li><a href="./index.php?view=home"><i class='fa fa-dashboard'></i> <span>Dashboard</span></a></li>
-<!-- <?php if(Core::$user->kind==1||Core::$user->kind==2):?>
+            <?php endif;?>
+<!-- <?php if (Core::$user->kind == 1 || Core::$user->kind == 2): ?>
                         <li><a href="./index.php?view=alerts"><i class='fa fa-bell-o'></i> <span>Alertas</span></a></li>
-                        <?php endif; ?> -->
+                        <?php endif;?> -->
                         <!-- <li><a href="./?view=sell"><i class='fa fa-usd'></i> <span>Vender</span></a></li> -->
                         <!--  <li><a href="./?view=cotizations"><i class='fa fa-square-o'></i> <span>Cotizaciones</span></a></li>-->
 
@@ -165,23 +167,26 @@
                 <li><a href="./?view=messages&opt=all">Mensajes</a></li>
               </ul>
             </li> -->
-            <li class="treeview <?php if(isset($_GET["view"]) && ($_GET["view"]=="sells"||$_GET["view"]=="bydeliver" ||$_GET["view"]=="bycob")){ echo "active"; }?>"   >
+            <?php if (Core::$user->kind == 1): ?>
+            <li class="treeview <?php if (isset($_GET["view"]) && ($_GET["view"] == "sells" || $_GET["view"] == "bydeliver" || $_GET["view"] == "bycob")) {echo "active";}?>"   >
               <a href="#"><i class='fa fa-shopping-cart'></i> <span>Ventas</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="./?view=sells">Ventas</a></li>
-<!-- ?php if(Core::$user->kind==1):?>
-       <li><a href="./?view=sellscredit">Ventas credito</a></li>
-   <?php endif; ?>
-                <li><a href="./?view=bydeliver">Por Entregar</a></li>
-                <li><a href="./?view=bycob">Por Cobrar</a></li> -->
+
+
+
+
+
               </ul>
             </li>
-            <?php if(Core::$user->kind==3):?>
-              <li><a href="./?view=inventary&stock=<?php echo StockData::getPrincipal()->id;?>"><i class='fa fa-area-chart'></i> <span>Inventario</span></a></li>
-            <?php endif; ?>
+              <?php endif;?>
+             <?php endif;?>
+            <?php if (Core::$user->kind == 3): ?>
+              <li><a href="./?view=inventary&stock=<?php echo StockData::getPrincipal()->id; ?>"><i class='fa fa-area-chart'></i> <span>Inventario</span></a></li>
+            <?php endif;?>
 
 
-            <!-- <?php if(Core::$user->kind==1 || Core::$user->kind==2):?>
+            <!-- <?php if (Core::$user->kind == 1 || Core::$user->kind == 2): ?>
             <li class="treeview">
               <a href="#"><i class='fa fa-clock-o'></i> <span>Compras</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
@@ -191,20 +196,22 @@
           <li><a href="./?view=topay">por Pagar</a></li>
               </ul>
             </li> -->
-            <?php if(Core::$user->kind==1):?>
+            <?php if (Core::$user->kind == 1): ?>
               <li><a href="./?view=products"><i class='fa fa-cutlery'></i> <span>Productos</span></a></li>
               <li><a href="./?view=devoluciones"><i class='fa fa-reply'></i> <span>Devoluciones</span></a></li>
               <li><a href="./?view=categories"><i class='fa fa-sitemap'></i> <span>Categorias</span></a></li>
               <li><a href="./?view=stocks"><i class='fa fa-building'></i> <span>Almacenes</span></a></li>
               <li><a href="./?view=boxhistory"><i class='fa fa-briefcase'></i><span>Cortes de Caja</span></a></li>
+               <?php endif;?>
+
             <!--             <li class="treeview">
               <a href="#"><i class='fa fa-database'></i> <span>Catalogos</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="./?view=products">Productos</a></li>
                 <li><a href="./?view=categories">Categorias</a></li>
-                 <li><a href="./?view=clients">Clientes</a></li> 
+                 <li><a href="./?view=clients">Clientes</a></li>
                 <li><a href="./?view=stocks">Almacenes</a></li>
-                 <li><a href="./?view=providers">Proveedores</a></li> 
+                 <li><a href="./?view=providers">Proveedores</a></li>
               </ul>
             </li>
           -->
@@ -221,16 +228,16 @@
             <li class="treeview">
               <a href="#"><i class='fa fa-area-chart'></i> <span>Inventario</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
-                <li><a href="./?view=inventary&stock=<?php echo StockData::getPrincipal()->id;?>">Inventario Principal</a></li>
+                <li><a href="./?view=inventary&stock=<?php echo StockData::getPrincipal()->id; ?>">Inventario Principal</a></li>
                 <li><a href="./?view=re">Abastecer</a></li>
-                <?php if(Core::$user->kind==1):?>
+                <?php if (Core::$user->kind == 1 || Core::$user->kind == 2): ?>
                   <li><a href="./?view=stocks">Inventarios</a></li>
                   <!-- <li><a href="./?view=selectstock">Traspasar</a></li> -->
                   <li><a href="./?view=dev">Devolucion</a></li>
-                <?php endif; ?>
+
               </ul>
             </li>
-            <?php if(Core::$user->kind==1):?>
+            <?php if (Core::$user->kind == 1): ?>
               <li class="treeview">
                 <a href="#"><i class='fa fa-file-text-o'></i> <span>Reportes</span> <i class="fa fa-angle-left pull-right"></i></a>
                 <ul class="treeview-menu">
@@ -241,7 +248,7 @@
                   <li><a href="./?view=popularproductsreport">Productos Populares</a></li>
                 </ul>
               </li>
-
+               <?php endif;?>
 
               <li class="treeview">
                 <a href="#"><i class='fa fa-cog'></i> <span>Administracion</span> <i class="fa fa-angle-left pull-right"></i></a>
@@ -253,12 +260,12 @@
 
                 </ul>
               </li>
-            <?php endif; ?>
-          <?php endif; ?>
-          <?php elseif(isset($_SESSION["client_id"])):?>
+
+          <?php endif;?>
+          <?php elseif (isset($_SESSION["client_id"])): ?>
             <li><a href="./index.php?view=clienthome"><i class='fa fa-dashboard'></i> <span>Dashboard</span></a></li>
             <li><a href="./?view=cotizations"><i class='fa fa-square-o'></i> <span>Cotizaciones</span></a></li>
-            <li class="treeview <?php if(isset($_GET["view"]) && ($_GET["view"]=="sells"||$_GET["view"]=="bydeliver" ||$_GET["view"]=="bycob")){ echo "active"; }?>"   >
+            <li class="treeview <?php if (isset($_GET["view"]) && ($_GET["view"] == "sells" || $_GET["view"] == "bydeliver" || $_GET["view"] == "bycob")) {echo "active";}?>"   >
               <a href="#"><i class='fa fa-shopping-cart'></i> <span>Mis Compras</span> <i class="fa fa-angle-left pull-right"></i></a>
               <ul class="treeview-menu">
                 <li><a href="./?view=sells">Todas</a></li>
@@ -275,7 +282,7 @@
   <?php endif;?>
 
   <!-- Content Wrapper. Contains page content -->
-  <?php if(isset($_SESSION["user_id"]) || isset($_SESSION["client_id"])):?>
+  <?php if (isset($_SESSION["user_id"]) || isset($_SESSION["client_id"])): ?>
   <div class="content-wrapper">
     <?php View::load("index");?>
   </div><!-- /.content-wrapper -->
@@ -286,8 +293,8 @@
     </div>
     <strong>Copyright &copy; 2019 <a href="http://vmcomp.mx/" target="_blank">VMComp</a></strong>
   </footer>
-  <?php else:?>
-    <?php if(isset($_GET["view"]) && $_GET["view"]=="clientaccess"):?>
+  <?php else: ?>
+    <?php if (isset($_GET["view"]) && $_GET["view"] == "clientaccess"): ?>
       <div class="login-box">
         <div class="login-logo">
           <a href="./"><img style="width: 80%;" src="logo.png"></a>
@@ -313,8 +320,8 @@
             </div>
           </form>
         </div><!-- /.login-box-body -->
-      </div><!-- /.login-box -->  
-      <?php else:?>
+      </div><!-- /.login-box -->
+      <?php else: ?>
         <div class="login-box">
             <div class="login-box-body">
             <center>
@@ -324,16 +331,16 @@
               <form action="./?action=processlogin" method="post">
                 <div class="form-group has-feedback">
                   <input type="text" name="username" required class="form-control inputlogin" placeholder="Usuario"/>
-                  
+
                 </div>
                 <div class="form-group has-feedback">
                   <input type="password" name="password" required class="form-control inputlogin" placeholder="Contraseña"/>
-                  
+
                 </div>
                 <div class="row">
 
                   <div class="col-xs-12">
-                   
+
                    <center>
                     <button type="submit" class="btnlogin">Acceder</button>
                     </center>
@@ -342,7 +349,7 @@
                 </div>
               </form>
             </div><!-- /.login-box-body -->
-          </div><!-- /.login-box -->  
+          </div><!-- /.login-box -->
         <?php endif;?>
       <?php endif;?>
 
